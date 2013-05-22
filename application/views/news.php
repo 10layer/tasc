8dded0d8de8d8aea1ca52bd24c32e4dbeb669940
j-norwood-young <jason@10layer.com>
2013-05-22 11:07:32 +0200

    Minor layout change

--- application/views/news.php
+++ application/views/news.php
@@ -2,16 +2,16 @@
 	$this->load->view("templates/header");
 ?>
 <div class="container body">
-	<div class="row underline">
-		<div class="span12">
+	<div class="row">
+		<div class="span11 offset1 underline">
 			<h1>African Story Challenge News</h1>
 		</div>
 	</div>
 	<?php
 	foreach($news as $item) {
 	?>
-	<div class="row underline">
-		<div class="span9 offset2">
+	<div class="row ">
+		<div class="span10 offset2 underline">
 			<h2><?= anchor($item->content_type."/".$item->_id, $item->title) ?></h2>
 			<?php
 			if (!empty($item->blurb)) {
