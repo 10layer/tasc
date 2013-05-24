7e87f3471934dc506dd452859bf41e1771d08584
j-norwood-young <jason@10layer.com>
2013-05-24 08:58:16 +0200

    Fix upload

--- application/controllers/apply.php
+++ application/controllers/apply.php
@@ -25,23 +25,25 @@
 				$fields = $_POST;
 				
 				$config['upload_path'] = "./content/".date("Y")."/".date("m")."/".date("d")."/";
+				if (!is_dir($config['upload_path'])) {
+					mkdir($config["upload_path"], 0755, true);
+				}
 				$config['allowed_types'] = 'doc|docx|pdf|jpg|png';
 				$config['encrypt_name'] = true;
 				$this->load->library('upload', $config);
 				foreach($_FILES as $key=>$file) {
 					if ($this->upload->do_upload($key)) {
 						$upload_data = $this->upload->data();
-						print_r($upload_data);
 						$fields[$key] = "/content/".date("Y")."/".date("m")."/".date("d")."/".$upload_data["file_name"];
 					} else {
 						$errors = $this->upload->display_errors();
-						print_r($errors);
+						//print_r($errors);
 					}	
 				}
 				foreach(array_keys($fields) as $key) {
 					$data["form"]->{$key}->value = $this->input->post($key);
 				}
-				print_r($fields);
+				//print_r($fields);
 				$url = $this->config->item("apiurl")."content/save?content_type=submission&api_key=6a07db80feac1dbb8127ee5ac72bcfb5";
 				$ch = curl_init($url);
 				curl_setopt($ch, CURLOPT_POST, 1);
