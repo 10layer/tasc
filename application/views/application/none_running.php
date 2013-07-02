fb9da27b7b8633422aa857625e7ae6750438711d
j-norwood-young <jason@10layer.com>
2013-07-02 11:18:38 +0200

    French title and date

--- application/views/application/none_running.php
+++ application/views/application/none_running.php
@@ -7,7 +7,10 @@
 	
 	<div class="alert alert-warning alert-block">
 		<h3>No competitions currently running</h3>
-		<p>Submissions for <strong><?= $next_competition->title ?></strong> will start on <?= date("l, j F, Y", $next_competition->submission_start_date) ?></p>
+		<?php
+			setlocale(LC_TIME, "fr_FR");
+		?>
+		<p>Submissions for <strong><?= $next_competition->french_title ?></strong> will start on <?= strftime("%A, %e %B, %Y", $next_competition->submission_start_date) ?></p>
 	</div>
 	
 </div>
